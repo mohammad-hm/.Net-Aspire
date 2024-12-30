362cfa7e0ab95aa0fdbc964ddbfbc9484bc7321e
mohammad hajimohammadi <hajimohammadi@amnpardaz.com>
2024-12-30 11:45:03 +0330

    feat: add new project to Aspire(redis service)

--- MyAspireApp.AppHost/Program.cs
+++ MyAspireApp.AppHost/Program.cs
@@ -1,7 +1,16 @@
+using Aspire.Hosting;
+using Aspire.Hosting.Redis;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.MyAspireApp_ApiService>("apiservice");
 
+// Add Redis
+var redis = builder.AddRedis("cache");
+
+// Add your Redis service and reference Redis
+var redisService = builder.AddProject<Projects.MyAspireApp_RedisService>("redisService")
+    .WithReference(redis);
 
 builder.AddProject<Projects.MyAspireApp_Web>("webfrontend")
     .WithExternalHttpEndpoints()
